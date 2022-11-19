f25b642b67a95d4eae0207c0da7126e387c07841
LAWAL <haydelawal@gmail.com>
2022-11-19 19:18:14 +0100

    workmanager notification functionality implemented

---
+++ CarbonFree/app/src/main/java/com/example/carbonfree/ui/fragments/main/details/DetailsFragment.kt
@@ -0,0 +1,37 @@
+package com.example.carbonfree.ui.fragments.main.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.carbonfree.databinding.FragmentDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailsFragment : Fragment() {
+
+    private lateinit var _binding: FragmentDetailsBinding
+    private val binding get() = _binding!!
+
+    private val args: DetailsFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+
+        binding.imageView.setImageResource(args.selectedCarbonFree.carbonFreeNotificationLargeIcon)
+        binding.textView.text = args.selectedCarbonFree.carbonFreeName.toString()
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding.root
+    }
+}
