44c9d1cb187b30e38284c92bb0d59474f7cc9d13
Jcquigee <gitaujaquiline@gmail.com>
2022-11-22 09:53:56 +0300

    Adding branches

--- CarbonFree/app/src/main/java/com/example/carbonfree/ui/ThirdFragment.kt
+++ CarbonFree/app/src/main/java/com/example/carbonfree/ui/ThirdFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.carbonfree.databinding.FragmentThirdBinding
+import com.example.carbonfree.databinding.FragmentThirdBindingq
 
 
 class ThirdFragment : Fragment() {
