568a440d91f5dd844d78929b7a882a88b0e02ce9
Jcquigee <gitaujaquiline@gmail.com>
2022-11-22 10:13:11 +0300

    making corrections on ThirdFragment file

--- CarbonFree/app/src/main/java/com/example/carbonfree/ui/ThirdFragment.kt
+++ CarbonFree/app/src/main/java/com/example/carbonfree/ui/ThirdFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.carbonfree.databinding.FragmentThirdBindingq
+import com.example.carbonfree.databinding.FragmentThirdBinding
 
 
 class ThirdFragment : Fragment() {
